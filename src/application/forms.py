36877645c65b88642bbcb6fb22079438a5ffe854
stevenj2019 <steven.j2019@pm.me>
2020-08-21 17:45:44 +0100

    split up html into templates

--- src/application/forms.py
+++ src/application/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm 
 from wtforms import StringField, PasswordField, SubmitField, SelectField
-from wtforms.validators import DataRequired, Email, Equalto
+from wtforms.validators import DataRequired, Email, EqualTo
 from application.models import Admin
 from flask_login import current_user 
 
@@ -9,11 +9,16 @@ class AdminLoginForm(FlaskForm):
     password = PasswordField('Password', validators=[DataRequired()])
     submit = SubmitField('')
 
-class NewAdminForm(FlaskForm):
+class AdminForm(FlaskForm):
     first_name = StringField('First Name:', validators=[DataRequired()])
     last_name = StringField('Last Name:', validators=[DataRequired()])
     email = StringField('Email', validators=[DataRequired(), Email()])
-    password = PasswordField('Password', validators=[DataRequired(), Equalto('confirm', message='Passwords must match')])
+    password = PasswordField('Password', validators=[DataRequired(), EqualTo('confirm', message='Passwords must match')])
     confirm = PasswordField('Password', validators=[DataRequired()])
     level = SelectField(choices=[], coerce=int, validators=[DataRequired()])
     submit = SubmitField('')
+
+class ResetPasswordForm(FlaskForm):
+    old_password = PasswordField('Old Password:', validators=[DataRequired()])
+    new_password = PasswordField('New Password:', validators=[DataRequired(), EqualTo('confirm', message='Password must match')])
+    confirm = PasswordField('Confirm Password:', validators=[DataRequired()])
--- src/application/routes.py
+++ src/application/routes.py
@@ -1,7 +1,7 @@
 from flask import render_template, redirect, url_for, request
 from flask_login import login_user, current_user, logout_user, login_required
 from application.models import Admin
-from application.forms import AdminLoginForm, NewAdminForm
+from application.forms import AdminLoginForm, AdminForm, ResetPasswordForm
 from application import app
 
 @app.route('/admin', methods=['GET', 'POST'])
@@ -9,7 +9,7 @@ def adm_login():
     form = AdminLoginForm()
     if form.validate_on_submit():
         admin = Admin.query.filter_by(email=form.email.data).first()
-        if user and bcrypt.check_password_hash(user.password, form.password.data):
+        if admin and bcrypt.check_password_hash(admin.password, form.password.data):
             login_user(user)
             return redirect(url_for('adm-home'))
         else:
@@ -21,23 +21,37 @@ def adm_login():
 def adm_home():
     month_revenue = 0
     annual_revenue = 0
-    return render_template('adminhome.html', month = month_revenue, annual = annual_revenue)
+    return render_template('adminhome.html', title = "Dashboard", month = month_revenue, annual = annual_revenue)
 
 #@login_required
 @app.route('/new-admin')
 def adm_new_admin():
-    form = NewAdminForm()
+    form = AdminForm()
     roles = (['admin', 'admin'])
-    if Admin.query.filter_by(user_id = current_user.user_id).first().level == 'owner':
-        roles.append(['owner', 'owner'])
+    #if Admin.query.filter_by(user_id = current_user.user_id).first().level == 'owner':
+        #roles.append(['owner', 'owner'])
     if form.validate_on_submit():
         Data = Admin(first_name = form.first_name.data, last_name = form.last_name.data, email = form.email.data, password = bcrypt.generate_password_has(form.password.data), level = form.level.data)
-        db.session.add(Data)
-        db.session.commit()
+        #db.session.add(Data)
+        #db.session.commit()
         return redirect(url_for('adm_home'))
     else:
         print(form.errors)
-    return render_template('newadmin.html', form = form)
+    return render_template('adminform.html', form = form, title = "New Admin", form_title ="Create a New Admin User")
+
+@app.route('reset-password')
+def adm_password_reset():
+    form = ResetPasswordForm()
+    admin = Admin.query.filter_by(id=current_user.id).first()
+    if form.validate_on_submit():
+        if admin and bcrypt.check_password_hash(admin.password, form.old_password.data):
+            admin.password = bcrypt.generate_password_hash(form.new_password.data)
+            db.session.commit()
+        else:
+            form.errors.append('incorrect password supplied')
+    else:
+        print(form.errors)
+    return render_template('resetpassword.html', form = form, title="Reset Password")
 
 #@login_required
 @app.route('/logout')
